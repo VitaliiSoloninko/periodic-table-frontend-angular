b5dc91b97bba2dfcf546a09c7038e553c470a59d
22051988Di <vitalii.soloninko@gmail.com>
2025-07-24 12:43:38 +0200

    Folder structure, font import, first component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { ElementsPageComponent } from './pages/elements-page/elements-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: ElementsPageComponent }];
---
+++ src/app/pages/elements-page/elements-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { TitleComponent } from '../../shared/title/title.component';
+
+@Component({
+  selector: 'app-elements-page',
+  imports: [TitleComponent],
+  templateUrl: './elements-page.component.html',
+  styleUrl: './elements-page.component.css',
+})
+export class ElementsPageComponent {}
