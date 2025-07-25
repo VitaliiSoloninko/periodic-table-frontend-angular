4dc69192dca45b839ce3566af79ee04044326fe4
22051988Di <vitalii.soloninko@gmail.com>
2025-07-25 11:48:02 +0200

    Fix element service url

--- src/app/services/element.service.ts
+++ src/app/services/element.service.ts
@@ -2,15 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ChemicalElement } from '../interfaces';
-import { ELEMENTS_URL } from '../urls';
+import { ELEMENT_SEARCH_URL, ELEMENTS_URL } from '../urls';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ElementService {
   constructor(private http: HttpClient) {}
-  private apiUrl =
-    'https://periodic-table-backend-nestjs-postgresql-production.up.railway.app';
 
   getAllElements(): Observable<ChemicalElement[]> {
     return this.http.get<ChemicalElement[]>(ELEMENTS_URL);
@@ -22,13 +20,7 @@ export class ElementService {
 
   searchByName(name: string): Observable<ChemicalElement[]> {
     return this.http.get<ChemicalElement[]>(
-      `${this.apiUrl}/elements-search/name?q=${name}`
-    );
-  }
-
-  searchBySymbol(symbol: string): Observable<ChemicalElement[]> {
-    return this.http.get<ChemicalElement[]>(
-      `${this.apiUrl}/elements-search/symbol?symbol=${symbol}`
+      `${ELEMENT_SEARCH_URL}/name?q=${name}`
     );
   }
 }
--- src/app/urls.ts
+++ src/app/urls.ts
@@ -3,5 +3,3 @@ const BASE_URL =
 
 export const ELEMENTS_URL = BASE_URL + 'elements';
 export const ELEMENT_SEARCH_URL = BASE_URL + 'elements-search';
-export const ELEMENTS_BY_NAME_URL = ELEMENT_SEARCH_URL + '/name';
-export const ELEMENTS_BY_SYMBOL_URL = ELEMENT_SEARCH_URL + '/symbol';
